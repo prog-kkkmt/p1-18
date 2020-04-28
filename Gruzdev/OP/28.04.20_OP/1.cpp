1e536ebe80c65f5db1cd1e3ed6359c82da21e1cd
R0m-4uk <64011212+R0m-4uk@users.noreply.github.com>
2020-04-28 18:46:22 +0300

    Update 1.cpp

--- Gruzdev/OP/28.04.20_OP/1.cpp
+++ Gruzdev/OP/28.04.20_OP/1.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 
-
+// Описана функция бинарного поиска
 int Bin_Search(int mas[], int key, int n){
    int mid, r = n - 1, l = 0;
   
@@ -18,7 +18,7 @@ int Bin_Search(int mas[], int key, int n){
 		l = ++mid;
 	else r = --mid;
    }
-   return -1;
+   return -1; // если введено неверное число(которого нет в массиве), то возвращается -1
 }
 
 int main(){
@@ -29,11 +29,13 @@ int main(){
 	cin >> quantity;
   int mas[quantity];
   	cout << "Хорошо, давайте теперь его заполним.. " << endl;
+	// Заполняем массив
 	for(int i = 0;i < quantity;i++){
 		cin >> n;
 		mas[i] = n;
 	}
 	int k;
+	// Сортируем массив(т.к. иначе не будет работать бинарный поиск)
 	for(int i = 0;i < quantity-1;++i)
 		for(int j = 0;j < quantity-1;++j)
 			if(mas[j] > mas[j + 1]){
@@ -41,13 +43,13 @@ int main(){
 				mas[j] = mas[j + 1];
 				mas[j + 1] = k;
 			}	
+	// Вывод массива(чтобы удостовериться, что он отсортирован)
 	for(int i = 0;i < quantity;i++)
-		cout << " |" << mas[i];
+		cout << "|" << mas[i] << "| ";
 
 	cout << endl << "Отлично! Теперь выполним бинарный поиск.." << endl;
 	cout << "Введите ключ ..";
-	cin >> key;
-	cout << Bin_Search(mas,key,quantity) << endl;
+	cin >> key; // Вводим число у которого хотим узнать позицию
 
 	if(Bin_Search(mas,key,quantity) == -1)
 		cout << "Мы не смогли поймать число, видимо оно знало, что мы прийдём за ним.." << endl;
