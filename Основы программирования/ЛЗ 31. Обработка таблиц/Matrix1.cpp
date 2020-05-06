1bdaec6fcd5f516f0e253cbd67fe356877af6e1d
RuLamed <lgusyatiner@mail.ru>
2020-05-06 11:10:36 +0300

    Add files via upload

---
+++ Основы программирования/ЛЗ 31. Обработка таблиц/Matrix1.cpp
@@ -0,0 +1,30 @@
+/* Programming Taskbook. Электронный задачник по программированию
+(c)  М. Э. Абрамян (Южный федеральный университет), 1998–2020 */
+// Двумерные массивы (матрицы)
+// http://ptaskbook.com/ru/tasks/matrix.php
+/* Matrix1. Даны целые положительные числа M и N. Сформировать целочисленную матрицу размера M * N,
+у которой все элементы I-й строки имеют значение 10*I (I = 1, ..., M). */
+/* Решение. Гусятинер Л.Б., МГОТУ ККМТ, 06.05.2020 */
+#include <iostream>
+using namespace std;
+
+int main() {
+	const size_t MAXN = 100;
+	size_t m, n;
+	cin >> m >> n;
+	int mat[MAXN][MAXN];
+	for (size_t i = 0; i < m; i++) {
+		int elem = 10 * i;
+		for (size_t j = 0; j < n; j++)
+			mat[i][j] = elem;
+	}
+	for (size_t i = 0; i < m; i++) {
+		for (size_t j = 0; j < n; j++) {
+			cout << mat[i][j];
+			if (j < n - 1)
+				cout << "\t";
+		}
+		cout << endl;
+	}
+	return 0;
+}
---
+++ Основы программирования/ЛЗ 31. Обработка таблиц/Matrix7.cpp
@@ -0,0 +1,31 @@
+/* Programming Taskbook. Электронный задачник по программированию
+(c)  М. Э. Абрамян (Южный федеральный университет), 1998–2020 */
+// Двумерные массивы (матрицы)
+// http://ptaskbook.com/ru/tasks/matrix.php
+/* Двумерные массивы (матрицы): вывод элементов
+/* Matrix7. Дана матрица размера M * N и целое число K (0<= K < M).
+Вывести элементы K-й строки данной матрицы. */
+/* Решение. Гусятинер Л.Б., МГОТУ ККМТ, 06.05.2020 */
+#include <iostream>
+using namespace std;
+
+int main() {
+	const size_t MAXN = 100;
+	size_t m, n;
+	size_t k;
+	cin >> m >> n;
+	cin >> k;
+
+	int mat[MAXN][MAXN];
+	for (size_t i = 0; i < m; i++)
+		for (size_t j = 0; j < n; j++)
+			cin >> mat[i][j];
+
+    for (size_t j = 0; j < n; j++) {
+        cout << mat[k][j];
+        if (j < n - 1)
+            cout << "\t";
+    }
+    cout << endl;
+	return 0;
+}
