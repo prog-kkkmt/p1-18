e8c70c6d5324ba3270917a1fa1a4e61c3be0ceea
nechalate <penpolpen@gmail.com>
2020-05-26 22:29:52 +0300

    lol

--- Программирование/ЛЗ-38/multifile_project/main.cpp
+++ Программирование/ЛЗ-38/multifile_project/main.cpp
@@ -18,6 +18,7 @@ vector<string> split_whitespace(string s)
 }
 
 // Меню
+/*
 void menu() {
     cout << "0. Выход\n";
     cout << "1. Загрузить из файла\n";
@@ -29,7 +30,7 @@ void menu() {
     cout << "7. Выдать свод по каждому Продавцу (по убыванию общей суммы)\n";
     cout << "8. Выдать общий список\n";
 }
-
+*/
 // // Выдать свод по каждому Продавцу (по убыванию общей суммы)
 // void print_salers(vOrders v) {
 //     vector <Saler> salers;
@@ -53,48 +54,63 @@ void menu() {
 // }
 
 // Выдать общий список
-void print_all(vOrders v) {
-    cout << "Номер\tПродавец\tПокупатель\tДата\tСумма\n";
+void print_all(vOrders & v) {
+    cout << "Number\tSeller\tCustomer\tDate\tSumm\n";
     for (size_t i = 0; i < v.size(); i++)
-        cout << v[i].order_id << "\t" << v[i].saler << "\t" << v[i].customer << endl;
+        cout << v[i].order_id << "\t" << v[i].saler << "\t" << v[i].customer << "\t"  << v[i].date.year << "." << v[i].date.month << "." << v[i].date.day << "\t" << v[i].cost << endl;
 }
 
 int main() {
-    system("chcp 1251>nul");
+    setlocale(LC_ALL, "Russian");
     vOrders v;
     string fname = "orders.txt";
     while (true) {
-        menu();
+       // menu();
         string command;
-        cin >> command;
+        cout << "> ";
+        getline(cin, command);
         vector<string> words = split_whitespace(command);
-        cout << words[0];
         if (words[0] == "exit") { // Выход
-
+            exit(0);
         }
         else if (words[0] == "load") { // Загрузить
-
+            load(v, fname);
         }
         else if (words[0] == "save") {  // Выгрузить
-
+            save(v, fname);
         }
-        else if (words[0] == "add") { // Добавить
+       else if (words[0] == "add") { // Добавить
+           if (words.size() < 5){
+            cout << "Too few arguments" << endl;
+            continue;
+           }
+  //         struct Date sock;
 
+ //           append();
         }
         else if (words[0] == "edit") { // Редактировать
-
+ //           edit();
         }
         else if (words[0] == "delete") { // Удалить
-            printf("delete");
+            if (words.size() < 2){
+            cout << "Too few arguments" << endl;
+            continue;
+            }
+            int id = stoi(words[1])-1;
+            del(v, id);
+
         }
         else if (words[0] == "sort") { // Сортировать
-
+            sort(v);
         }
         else if (words[0] == "report") { // Отчет
-
+   //         print_salers();
         }
         else if (words[0] == "all") { // общий список
-
+            print_all(v);
+        }
+        else if (words[0] == "help") {
+            cout << "available command's: load, save, add, edit, delete, sort, report, all\n";
         }
     }
     return 0;
