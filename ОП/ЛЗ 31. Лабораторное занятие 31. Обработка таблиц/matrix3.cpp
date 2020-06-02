f2f138c60238dbdcf2195ba6f718888c3503d96f
NikolayM17 <64014097+NikolayM17@users.noreply.github.com>
2020-06-02 16:01:33 +0300

    Add files via upload

---
+++ ОП/ЛЗ 31. Лабораторное занятие 31. Обработка таблиц/matrix3.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+/*
+Matrix3. Даны целые положительные числа M, N и набор из M чисел.
+Сформировать матрицу размера M × N, у которой в каждом столбце
+содержатся все числа из исходного набора (в том же порядке).
+*/
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	int M, N;
+	std::cin >> M >> N;
+	//array:
+	std::vector <int> arr(M);
+
+	int i, j;
+	//matrix:
+	std::vector <std::vector <int> > mat = std::vector <std::vector <int> >(M, std::vector <int>(N));
+	
+	for (i = 0; i < M; i++)
+	{
+		std::cin >> arr[i];
+	}
+
+	for (i = 0; i < M; i++)
+	{
+		for (j = 0; j < N; j++)
+		{
+			std::cout << arr[i] << '\t';
+		}
+		std::cout << std::endl << std::endl;
+	}
+
+	return 0;
+}
