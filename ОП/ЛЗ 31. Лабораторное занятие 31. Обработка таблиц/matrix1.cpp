a138c94843e7ceceab69bd15044001934d393303
NikolayM17 <64014097+NikolayM17@users.noreply.github.com>
2020-06-02 13:54:25 +0300

    Add files via upload

---
+++ ОП/ЛЗ 31. Лабораторное занятие 31. Обработка таблиц/matrix1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+/*Matrix1. Даны целые положительные числа M и N.
+Сформировать целочисленную матрицу размера M × N,
+у которой все элементы i-й строки имеют значение
+10·i (i = 1, …, M).*/
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	int n;
+	cin >> n;
+
+	const int M = 3;
+	const int N = 5;
+
+	int mat[M][N];
+
+	int i, j;
+
+	for (i = 0; i < M; i++)
+		for (j = 0; j < N; j++)
+			cin >> mat[i][j];
+
+	for (i = 0; i < M; i++)
+	{
+		for (j = 0; j < N; j++)
+			if (i == n)
+			{
+				mat[i][j] *= 10;
+				cout << mat[i][j] << '\t';
+			}
+			else
+				cout << mat[i][j] << '\t';
+		cout << endl;
+	}
+	return 0;
+}
---
+++ ОП/ЛЗ 31. Лабораторное занятие 31. Обработка таблиц/matrix2.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <vector>
+#include <array>
+/*
+Matrix2. Даны целые положительные числа M и N.
+Сформировать целочисленную матрицу размера M × N,
+у которой все элементы J-го столбца имеют значение
+5·J (J = 1, …, N).
+*/
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	
+	int M;
+	int N;
+	std::cin >> M >> N;
+
+	int i, j;
+
+	int n;
+	std::cin >> n;
+
+	//int mat[M][N];
+	std::vector<std::vector<int> > mat = std::vector<std::vector<int> >(M, std::vector<int>(N));// Вектор <вектор <формат>  >(Число, Вектор(Число))
+																							    //						   (в скобках - размер vec)
+	
+	for (i = 0; i < M; i++)
+		for (j = 0; j < N; j++)
+			std::cin >> mat[i][j];
+
+	for (i = 0; i < M; i++)
+	{
+		for (j = 0; j < N; j++)
+		{
+			if (j == n)
+			{
+				mat[i][j] *= 5;
+				std::cout << mat[i][j] << '\t';
+			}
+			else
+				std::cout << mat[i][j] << '\t';
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}
+
+
+
