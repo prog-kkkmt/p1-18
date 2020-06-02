74b954968f5d3d848bd6e993da346e26e74d3875
Ysezu <64014158+Ysezu@users.noreply.github.com>
2020-06-02 21:29:23 +0300

    Add files via upload

---
+++ OP/22 04/Text5.cpp
@@ -0,0 +1,15 @@
+/**Text5. Дана строка S и текстовый файл. Добавить строку S в конец файла.*/
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+	ofstream file("text.txt", ios::app);
+	string s;
+	getline(cin, s);
+	file << s << endl;
+	file.close();
+	return 0;
+}
