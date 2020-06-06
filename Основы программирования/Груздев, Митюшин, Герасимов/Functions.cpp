5abedbd98a5a51d7dbcdca9f415c804bdc4cd279
icefantik <43622554+icefantik@users.noreply.github.com>
2020-06-06 13:42:59 +0300

    Add files via upload

---
+++ Основы программирования/Груздев, Митюшин, Герасимов/Functions.cpp
@@ -0,0 +1,219 @@
+/** Команда состоит из 3-х человек: Груздев Роман, Герасимов Дмитрий, Петр Митюшин.
+П1-18, 05.06.2020 г.
+*/
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <fstream>
+#include "func.h"
+
+using namespace std;
+
+// 1.Загрузить из файла
+void load(vector<Nomination> *v, string fname) {
+    Nomination nom;
+    ifstream in(fname);
+
+    char delim;
+    v->clear();
+    while (in >> nom.nom_id >> nom.name >> nom.pr >> nom.date.day >> delim >> nom.date.month >> delim >> nom.date.year >> nom.cost >> nom.t >> nom.number){
+	v->push_back(nom);
+    }
+	in.close();
+   cout << endl;
+}
+
+// 2.Выгрузить в файл
+///Для определения табуляций (используется в 2,7,8)
+void numCh(string s1, string &t1, string s2, string &t2){
+	char num = s1.size();
+	//t1
+	if (num > 7)
+		t1 = "\t";
+	else
+		t1 = "\t\t";
+	//t2
+	if ((s2.size() + num) > 9)
+		t2 = "\t";
+	else
+		t2 = "\t\t";	
+}
+//input.txt
+void save(vNom &v, string fname_o){
+	ofstream out(fname_o, ios::app);
+
+	string t1, t2;
+	for(size_t i = 0; i != v.size(); i++){
+		numCh(v[i].name, t1, v[i].pr, t2);
+		out << v[i].nom_id << "  \t"
+			<< v[i].name << "  " << t1
+			<< v[i].pr << "   " << t2
+			<< v[i].date.day << "." << v[i].date.month << "." << v[i].date.year << "  \t"
+			<< v[i].cost << "\t\t"
+			<< v[i].t << "\t"
+			<< v[i].number << endl;
+	}
+	out << endl;	
+}
+
+// 3.Добавить новый заказ
+void append(vNom *v){
+	Nomination nom;
+	char delim;
+        while(cin >> nom.nom_id >> nom.name >> nom.pr >> nom.date.day >> delim >> nom.date.month >> delim >> nom.date.year >> nom.cost >> nom.t){
+
+        v->push_back(nom);
+	break;
+	}
+	cout << endl;
+}
+
+// 4.Редактировать сумму заказа
+void edit(vNom &v, size_t idx){
+	std::cout << "Введите новую сумму: ";
+	double ed_cost;
+	std::cin >> ed_cost;
+	v[--idx].cost = ed_cost;
+	cout << endl;
+}
+
+// 5.Удалить заказ
+void del(vNom *v, size_t idx){
+	v->erase(v->begin() + (--idx));
+	cout << endl << "Изделие удалено!" << endl;
+}
+
+// 6.Сортировать по дате 
+bool comp_year (const Nomination& a, const Nomination& b) { return (a.date.year < b.date.year); }
+
+bool comp_month (const Nomination& a, const Nomination& b) { return (a.date.month < b.date.month); }
+
+bool comp_day (const Nomination& a, const Nomination& b) { return (a.date.day < b.date.day); }
+
+void sort(vNom &v){
+
+	sort(v.begin(), v.end(), comp_year);
+	sort(v.begin(), v.end(), comp_month);
+	sort(v.begin(), v.end(), comp_day);
+	cout << endl;
+}
+
+
+// 7.Выдать общий список
+void print_all(vNom v){
+	cout << "ID\tНаименование\tПроизводитель\tДата\t\tСумма\tЕсть ли на складе\tКоличество\n";
+	string t1, t2;
+	for(size_t i = 0; i != v.size(); i++){
+		numCh(v[i].name, t1, v[i].pr, t2);
+		cout << v[i].nom_id << "  \t"
+			<< v[i].name << "  " << t1
+			<< v[i].pr << "   " << t2
+			<< v[i].date.day << "." << v[i].date.month << "." << v[i].date.year << "  \t"
+			<< v[i].cost << "\t\t"
+			<< v[i].t << "\t\t"
+			<< v[i].number << endl;
+	}
+	cout << endl;		
+}
+
+// 8. Выдать товар по максимуму или по сумме
+
+//Макс для количества
+void maxx1(vNom v, size_t &index){
+	size_t num_v = v.size();
+	int elem_max = v[0].number;
+	for (size_t i = 0; i < num_v; ++i){
+    	if (v[i].number > elem_max){
+            elem_max = v[i].number;
+            index = i;
+        }
+    }
+}
+//Макс для суммы
+void maxx2(vNom v, size_t &index){
+	size_t num_v = v.size();
+	int elem_max = v[0].cost;
+	for (size_t i = 0; i < num_v; ++i){
+    	if (v[i].cost > elem_max){
+            elem_max = v[i].cost;
+            index = i;
+        }
+    }
+}
+
+//Мин для количества
+void minn1(vNom v, size_t &index){
+	size_t num_v = v.size();
+	int elem_min = v[0].number;
+	for (size_t i = 0; i < num_v; ++i){
+    	if (v[i].number < elem_min){
+            elem_min = v[i].number;
+            index = i;
+        }
+    }
+}
+//Мин для суммы
+void minn2(vNom v, size_t &index){
+	size_t num_v = v.size();
+	int elem_min = v[0].cost;
+	for (size_t i = 0; i < num_v; ++i){
+    	if (v[i].cost < elem_min){
+            elem_min = v[i].cost;
+            index = i;
+        }
+    }
+}
+
+//Общая
+void allMM(vNom v, int k){
+    size_t index = 0;
+	string t1, t2;
+	
+	if (k == 1)
+		maxx1(v, index);
+	else if (k == 2)
+		maxx2(v, index);
+	else if (k == 3)
+		minn1(v, index);
+	else
+		minn2(v, index);		
+
+	numCh(v[index].name, t1, v[index].pr, t2);
+    cout << v[index].nom_id << "  \t"
+			<< v[index].name << "  " << t1
+			<< v[index].pr << "   " << t2
+			<< v[index].date.day << "." << v[index].date.month << "." << v[index].date.year << "  \t"
+			<< v[index].cost << "\t\t"
+			<< v[index].t << "\t"
+			<< v[index].number << endl
+			<< endl;
+}
+
+//Главная
+void maxxMinn(vNom v){
+    int k = -1;
+	while(k){
+		cout << "0. Выход" << endl
+				<< "1. Максимум по количеству" << endl
+				<< "2. Максимум по сумме" << endl
+				<< "3. Минимум по количеству" << endl
+				<< "4. Минимум по сумме" << endl
+				<< endl;
+		cin >> k;
+		if (k > 4 || k < 0)
+			cout << "Error. Диапазон меню: 0..4" << endl;
+		else if (k)
+			allMM(v, k);
+	}
+}
+
+
+
+
+
+
+
+
+
+
