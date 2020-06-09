1483222c54e41423f9fad21371ce3397bf9759ee
Alex <52524449+kmem1@users.noreply.github.com>
2020-06-09 18:17:47 +0300

    Add files via upload

--- C++/Straustrup/Vector.cpp
+++ C++/Straustrup/Vector.cpp
@@ -13,6 +13,30 @@ Vector::Vector(std::initializer_list<double> lst)
     std::copy(lst.begin(), lst.end(), elem);
   }
 
+Vector::Vector(const Vector& a) 
+: elem{new double[a.sz]}, sz{a.sz}
+{
+  for(int i = 0; i < sz; i++)
+    elem[i] = a.elem[i];
+}
+
+Vector::Vector(Vector&& a)
+: elem{a.elem}, sz{a.sz}
+{
+  a.elem = nullptr;
+  a.sz = 0;
+}
+
+Vector& Vector::operator=(const Vector &a){
+  double* p = new double[a.sz];
+  for(int i = 0; i < a.sz; i++)
+    p[i] = a.elem[i];
+  delete[] elem;
+  elem = p;
+  sz = a.sz;
+  return *this;
+}
+
 Vector::~Vector(){delete[] elem;}
 
 double& Vector::operator[](int i){
@@ -20,12 +44,27 @@ double& Vector::operator[](int i){
   return elem[i];
 }
 
+Vector& Vector::operator=(Vector &&a){
+  elem = a.elem;
+  sz = a.sz;
+  a.elem = nullptr;
+  a.sz = 0;
+  return *this;
+}
+
 int Vector::size() const {return sz;}
 
+void Vector::print_elems() const{
+  for(int i = 0; i < sz; i++)
+    std::cout << elem[i] << ' ';
+  std::cout << std::endl; 
+}
+
 void vector_test(){
-  Vector a = {1.2,3.4,5.6};
-  Vector b(5);
-  b[0] = 1;
-  for (int i = 0; i < a.size(); i++)
-    std::cout << a[i] + b[0] << std::endl;
+  Vector a = {1,2,3,4};
+  Vector b = {1,3,4,5};
+  a = b;
+  a[2] = 54;
+  a.print_elems();
+  b.print_elems();
 }
--- C++/Straustrup/Vector.h
+++ C++/Straustrup/Vector.h
@@ -2,9 +2,18 @@ class Vector {
   public:
     Vector(int s);
     Vector(std::initializer_list<double>);
+
+    Vector(const Vector& a);            //copy constructor
+    Vector& operator=(const Vector& a); //copy assignment
+
+    Vector(Vector&& a);                //move constructor
+    Vector& operator=(Vector&& a);     //move assignment
+
     ~Vector();
+
     double& operator[](int i);
     int size() const;
+    void print_elems() const;
   private:
     double * elem;
     int sz;
