834e4c4e75ceea93ee1bf137b452eb05baeace95
icefantik <43622554+icefantik@users.noreply.github.com>
2020-06-16 01:50:04 +0300

    Add files via upload

---
+++ Основы программирования/02.06.20_ОП(Лекция 30)/learn_class2_matrix.h
@@ -0,0 +1,20 @@
+#ifndef _matrix_
+#define _matrix_
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctime>
+class CMatrix
+{
+private:
+    int M;
+    int N;
+    int **ppx;
+
+public:
+    CMatrix(int m = 1, int n = 1);
+    ~CMatrix();
+    void Gen_elem();
+    void Print_elem();
+};
+#endif
---
+++ Основы программирования/02.06.20_ОП(Лекция 30)/main.cpp
@@ -0,0 +1,15 @@
+#include "learn_class2_matrix.h"
+
+void main(void)
+{
+    CMatrix matr(12, 7);
+
+    matr.Gen_elem();
+    matr.Print_elem();
+
+    CMatrix matrica;
+
+    matrica.Gen_elem();
+    matrica.Print_elem();
+}
+
