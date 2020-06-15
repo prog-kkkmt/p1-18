cbd347f1c47cbf7b1c9e5f5286573548d6f3d086
icefantik <43622554+icefantik@users.noreply.github.com>
2020-06-16 01:15:04 +0300

    Add files via upload

---
+++ Основы программирования/27.03.20-ОП(Лекция 14)/ptr_st.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Date
+{
+    int year;
+    char month;
+    char day;
+};
+
+int main(){
+    struct Date oneDay;
+    struct Date *ptrDate;
+    int days = 0;
+    scanf("%d%hhd%hhd",&(oneDay.year), &(oneDay.month), &(oneDay.day));
+    ptrDate = &oneDay;
+    days += ptrDate->year * 12 * 30;
+    days += ptrDate->month * 30;
+    days += ptrDate->day;
+    printf("oneDay: %04d.%02hhd.%02hhd\n",ptrDate->year, ptrDate->month, ptrDate->day);
+
+    printf("дней с начала отсчета: %d\n",days);
+    return 0;
+}
