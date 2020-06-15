f978988766f9d09b584858de403c69124687fba8
icefantik <43622554+icefantik@users.noreply.github.com>
2020-06-16 01:37:25 +0300

    Add files via upload

---
+++ Основы программирования/18.04.20_ОП_4Пара(Лекция 18.04.20)/Lesson2.8_4.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Hi, " << name << endl;
+    return 0;
+}
