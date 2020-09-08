b1f7bf85d01b36c42faf1dc1f173153de9160ddc
IOrito <valerarogolev1@gmail.com>
2020-09-08 14:40:02 +0300

    add Stepik

---
+++ Системное/Тренажер для решения задач на языке с/1.3.1.c
@@ -0,0 +1,19 @@
+/** Выполнид Роголев В.А.
+Напишите программу, которая выводит размеры типов данных, указанных ниже.
+char, char*, char**
+int, int*, int**
+float, float*, float**
+double, double*, double**
+
+Запятые, пробелы и переносы строк сохранить. В разных системах эти значения могу варьироваться, поэтому лучше воспользоваться встроенным оператором.
+*/
+
+#include <stdio.h>
+
+int main(){
+    printf("%d, %d, %d\n", sizeof(char), sizeof(char*), sizeof(char**));
+    printf("%d, %d, %d\n", sizeof(int), sizeof(int*), sizeof(int**));
+    printf("%d, %d, %d\n", sizeof(float), sizeof(float*), sizeof(float**));
+    printf("%d, %d, %d\n", sizeof(double), sizeof(double*), sizeof(double**));
+    return 0;
+}
