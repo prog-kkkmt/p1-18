f39a7495ee6f7a43dd7a32cc4d3d3fd26e8c7726
NikolayM17 <64014097+NikolayM17@users.noreply.github.com>
2020-10-26 15:24:34 +0300

    Add files via upload

---
+++ Алгоритмы/Пузырёк.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int main()
+{
+	int a[5];
+
+	for (int i = 0; i < 5; i++) std::cin >> a[i];
+
+
+	for (int i = 1; i < 5; i++)
+		for (int j = 0; j < 4; j++)
+		{
+			if (a[j] > a[j + 1])
+			{
+				int temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
+			}
+		}
+
+	for (auto i : a) std::cout << i << ' ';
+
+	std::cout << std::endl;
+
+	return 0;
+}
