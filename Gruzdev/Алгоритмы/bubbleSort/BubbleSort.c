6fe61c0d251712c3c03628512aadce97d5ce2d80
R0m-4uk <64011212+R0m-4uk@users.noreply.github.com>
2020-10-26 15:37:52 +0300

    Update BubbleSort.c

--- Gruzdev/Алгоритмы/bubbleSort/BubbleSort.c
+++ Gruzdev/Алгоритмы/bubbleSort/BubbleSort.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 #define MAX 7
-int main(){
+
+void BubbleSort(int arr[]){
     int a = 0;
-    int arr[] = {-4, 1, 7, 154, 90, -2, -2};
-        for(int i = 0; i != MAX; i++)
+    for(int i = 0; i != MAX; i++)
             for(int j = i+1; j != MAX; j++)
                 if(arr[i] > arr[j]){
                     a = arr[i];
                     arr[i] = arr[j];
                     arr[j] = a;
                 }
-
     for(int i = 0; i != MAX; i++)
         printf("%d ", arr[i]);
+}
 
+
+int main(){
+    int arr[] = {-4, 1, 7, 154, 90, -2, 34};
+    BubbleSort(arr);
+    
     return 0;
 }
