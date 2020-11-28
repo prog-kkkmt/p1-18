f47f8894d5e6bfea117a668804baa82302267b75
icefantik <43622554+icefantik@users.noreply.github.com>
2020-11-28 14:52:34 +0300

    Update stack.c

--- Алгоритмы/ЛЗ 27/stack.c
+++ Алгоритмы/ЛЗ 27/stack.c
@@ -2,21 +2,19 @@
 #include "stack.h"
 #define LEN 100
 
-//struct Stack stack[LEN];
-
-void push(Stack *stack, float elem)
+void push(Stack *stack, float elem) //добавляем элемент в стек
 {
 	stack->stack[stack->len_stack] = elem;
 	stack->len_stack++;
 }
 
-void pushStackNumbers(Stack *stack, int elem)
+void pushStackNumbers(Stack *stack, int elem) //добавляем номер элемета в стек
 {
 	stack->stack_numbers[stack->len_stack_num] = elem;
 	stack->len_stack_num++;
 }
 
-void init(Stack *stack)
+void init(Stack *stack) // инициализируем длины стеков для того чтобы в дальнейшем считать количество эелементов стека
 {
 	stack->len_stack_num = 0;
 	stack->len_stack = 0;
