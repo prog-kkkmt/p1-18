b737cfb1a7968fc2a073401d8795031df8c53efc
Nikita-Gusev1 <64073744+Nikita-Gusev1@users.noreply.github.com>
2020-12-16 16:25:11 +0300

    Add files via upload

---
+++ Практика_3_курс/К14/Задание 1/14_1(2).py
@@ -0,0 +1,16 @@
+#Выполнили: Гусев Никита и Цыпков Илья
+
+#Дана строка S и текстовый файл. Заменить в файле все пустые строки на строку S.
+
+file =open('text2.txt', 'r')
+file1 =open('text3.txt', 'a')
+s = input()
+for line in file:
+    if line == '\n':
+        file1.write(s + '\n')
+    else:
+        file1.write(line)
+    print(line)
+file.close()
+file1.close()
+    
