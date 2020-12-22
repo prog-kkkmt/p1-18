b97a37f7ea03959cee42b8d7c275c6b8725aaa85
Nikita-Gusev1 <64073744+Nikita-Gusev1@users.noreply.github.com>
2020-12-22 10:25:22 +0300

    Add files via upload

---
+++ Экзамен/Задание 1/зад 1.py
@@ -0,0 +1,22 @@
+#Выполнил: Гусев Никита 
+
+#Дан неупорядоченный файл целых чисел. Реализовать функцию поиска
+#максимального элемента. Оценить число операций присвоения в лучшем, худшем
+#и среднем случаях
+
+def max_elem(a):
+    return max(a)
+
+f1 = open('input.txt', 'r')
+lst = []
+for line in f1:
+    x = line.split()
+    i = 0
+    while i < len(x):
+        x[i] = int(x[i])
+        lst.append(x[i])
+        i+=1
+            
+print(max_elem(lst))
+f1.close()
+
