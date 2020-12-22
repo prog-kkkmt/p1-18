0856f5f93557e65c62528f75fa9c1060b8a1d819
batrakopter <64074288+batrakopter@users.noreply.github.com>
2020-12-23 02:42:23 +0300

    Add files via upload

---
+++ Практика/+16/К12_1/K12_1_2.py
@@ -0,0 +1,8 @@
+'''
+Задание 2. https://pythontutor.ru/lessons/sets/problems/number_of_coincidental/
+Задача «Количество совпадающих чисел»
+Условие. Даны два списка чисел. Посчитайте, сколько чисел содержится одновременно как
+в первом списке, так и во втором.
+'''
+
+print(len(set(input().split()) & (set(input().split()))))
