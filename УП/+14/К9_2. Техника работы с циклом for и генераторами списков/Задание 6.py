5b5ab4eabcd74ac7a2caadeca9632645f5c58420
icefantik <43622554+icefantik@users.noreply.github.com>
2021-04-13 11:21:51 +0300

    Update Задание 6.py

--- УП/+14/К9_2. Техника работы с циклом for и генераторами списков/Задание 6.py
+++ УП/+14/К9_2. Техника работы с циклом for и генераторами списков/Задание 6.py
@@ -11,7 +11,6 @@ index_app = 0
 for i in range(m):
     for j in range(m):
         if (j < index_app):
-            #print(index_app)
             mas[i][j] = 0
     index_app += 1
 
@@ -19,32 +18,3 @@ for i in range(m):
     for j in range(m):
         print(mas[i][j], end=" ")
     print()
-
-"""
-5
-5
-8
-8
-9
-6
-5
-4
-9
-7
-9
-9
-4
-3
-2
-4
-3
-5
-1
-6
-3
-1
-2
-5
-6
-9
-"""
