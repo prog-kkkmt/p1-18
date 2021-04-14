d54ea41ac504b7d12ea2229c26a26dc846d504bd
Nogad <44202517+KanaAnaberal@users.noreply.github.com>
2021-04-14 11:57:25 +0300

    Update 1.py

--- УП/K15.2/1.py
+++ УП/K15.2/1.py
@@ -1,3 +1,6 @@
+'''
+Задача №1. Контейнерные типы данных модуля collections. Замена значений.
+'''
 from collections import defaultdict
 a = defaultdict(set, **{"day":{"night"}, "night": {"month"}})
 a["month"].add("day")
