3defad84554391413e1eb7add4f36ca3b018807f
R0m-4uk <64011212+R0m-4uk@users.noreply.github.com>
2021-04-14 11:35:50 +0300

    Update K14_1_5.py

--- УП/+17/K14_1/K14_1_5/K14_1_5.py
+++ УП/+17/K14_1/K14_1_5/K14_1_5.py
@@ -10,14 +10,14 @@ Text53. Дан текстовый файл. Создать символьный 
 from string import punctuation
 
 with open("file.txt", "r") as fin:
-    list = [line for line in fin]
+    list_ = [line for line in fin]
 
 punc = set()
 
-for i in range(len(list)):
-    for j in range(len(list[i])):
-        if list[i][j] in punctuation:
-            punc.add(list[i][j])
+for i in range(len(list_)):
+    for j in range(len(list_[i])):
+        if list_[i][j] in punctuation:
+            punc.add(list_[i][j])
 
 fout = open("file2.txt", "w")
 fout.write("Знаки пунктуации:\n"+str(punc))
