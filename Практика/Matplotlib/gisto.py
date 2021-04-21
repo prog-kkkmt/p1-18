5aa3c1a4a80fa26059a8917153aa2b29d0900099
Nechalate <64017374+Nechalate@users.noreply.github.com>
2021-04-21 11:47:56 +0300

    Add files via upload

---
+++ Практика/Matplotlib/gisto.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+ 
+values = [0, 1.2, 1.3, 1.9, 4.3, 2.5, 2.7, 4.3, 1.3, 3.9] 
+plt.hist(values, bins = 4) 
+plt.show()
+
---
+++ Практика/Matplotlib/krivie.py
@@ -0,0 +1,12 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+ 
+X = np.linspace(-np.pi, np.pi, 256, endpoint=True) 
+cos, sin = np.cos(X), np.sin(X)
+ 
+plt.plot(X, cos) 
+plt.plot(X, sin)
+ 
+plt.show()
+
+
