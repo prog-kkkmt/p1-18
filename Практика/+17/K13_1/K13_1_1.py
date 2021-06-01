3535ee6da210c6ca7000cb7bffde4064780da306
batrakopter <64074288+batrakopter@users.noreply.github.com>
2021-06-01 21:35:42 +0300

    Delete Практика directory

--- Практика/+17/K13_1/K13_1_1.py
+++
@@ -1,7 +0,0 @@
-'''
-Задание 1. https://stepik.org/lesson/193753/step/4?unit=168148
-Вывести чётные
-Необходимо вывести все четные числа на отрезке [a; a * 10].
-'''
-a =int(input())
-print(tuple([i for i in range(a+a%2,a*10+1,2)]))
